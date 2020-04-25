89fac76120844dc34ba1adeae9cb01887c9ac182
JSKeum <JSKeum2512@gmail.com>
2020-04-26 00:21:51 +0900

    main page UI modified in main.css

--- draft-project/src/components/main/main.tsx
+++ draft-project/src/components/main/main.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import './main.css';
 
 class Main extends React.Component {
   render() {
     return(
-    <div className="main-ui">
-      <Header className="main-header" title="공명반 개발 스터디"/>
-      <main>
-        <StudyInfo className="left-info" />
-        <Articles className="right-articles" />  
-      </main>     
-    </div>
+      <div className="main-ui">
+        <Header className="main-header" title="공명 개발 스터디"/>
+        <main>
+          <StudyInfo className="left-info" />
+          <Articles className="right-articles" />  
+        </main>     
+      </div>
     );
   }
 }
@@ -21,7 +22,8 @@ function Header(props: Text) {
   return (
     <header className={props.className}>
       <h1>{props.title}</h1>
-      <p>Since. 2020.04.13</p>
+      {/* 소개글 넣기 */}
+      <p>Since. 2020.04.12</p>
     </header>
   );
 }
@@ -30,7 +32,7 @@ function StudyInfo(props: Text) {
   return (
     <div className={props.className}>
       <section>
-        <h2>스터디 멤버</h2>
+        <h2 className="deep-orange">스터디 멤버</h2>
         <ul>
           <li>다빈</li>
           <li>민재</li>
@@ -40,17 +42,17 @@ function StudyInfo(props: Text) {
         </ul>
       </section>
       <section>
-        <h2>Github</h2>
+        <h2 className="deep-orange">Github</h2>
         <ul>
-          <li>https://github.com/gongmyeong-study</li>
+          <li><a href="https://github.com/gongmyeong-study">깃헙 방문하기</a></li>
         </ul>
       </section>
       <section>
-        <h2>Slack</h2>
+        <h2 className="deep-orange">Slack</h2>
         <ul>
-          <li>https://gongmyeongstudy.slack.com</li>       
+          <li><a href="https://gongmyeongstudy.slack.com">슬랙 방문하기</a></li>       
         </ul>
-      </section>
+      </section>  
     </div>
   );
 }
@@ -60,8 +62,15 @@ function Articles(props: Text) {
     <div className={props.className}>
       <article>
         <h2>프로그래밍 기본 익히기</h2>
-        <h2>실습 1.</h2>
-        <h4>소문자를 입력 받아 대문자 출력하는 프로그램 만들기</h4>
+        <div className="exercise">
+          <h2 className="soft-orange">실습 1.</h2>
+          <p>소문자를 입력 받아 대문자 출력하는 프로그램 만들기</p>
+          <h2 className="soft-orange">실습 2.</h2>
+          <p>골드바흐의 추측을 검증하는 프로그램 만들기(N은 200000 이하)</p>
+        </div>
+      </article>
+      <article>
+        <h2>깃헙 사용하기</h2>
       </article>
     </div>
   );
@@ -69,6 +78,5 @@ function Articles(props: Text) {
 
 interface Text {
   title?: string;
-  text?: string;
   className?: string;
 }
