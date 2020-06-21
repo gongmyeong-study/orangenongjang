674b65310983832613141bc840bfefad689e432c
davin111 <bdv111@snu.ac.kr>
2020-06-21 07:17:17 +0000

    deploy

--- nongjang/nongjang/settings.py
+++ nongjang/nongjang/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = '%$(uu1zk1f4*8wnljep5ug(5t7*2u3+&exurk*0t+af56vbued'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'orangenongjang.com',
+    'www.orangenongjang.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -130,4 +135,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/django_static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
