e2d39778d18878bb2468b3d17722c16140644c74
Davin Byeon <bdv111@snu.ac.kr>
2020-07-13 23:29:01 +0900

    login api call with redux (#9)
    * login api call
    
    * loged in => logged in

--- orange/src/App.tsx
+++ orange/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { ConnectedRouter } from 'connected-react-router';
+
 import { Main } from './containers';
 import './App.css';
 
-function App() {
+interface Props {
+  history: any;
+}
+
+function App(props: Props): JSX.Element {
   return (
     <div className="App">
-      <Main />
+      <ConnectedRouter history={props.history}>
+        <Main />
+      </ConnectedRouter>
     </div>
   );
 }
--- orange/src/components/Articles/Articles.tsx
+++ orange/src/components/Articles/Articles.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { ArticleProps } from '../Article/Article';
+import Login from '../Login/Login';
 
 function Articles(props: ArticleProps) {
     return (
       <div className={props.className}>
         <article>
+          <Login />
         </article>
       </div>
     );
