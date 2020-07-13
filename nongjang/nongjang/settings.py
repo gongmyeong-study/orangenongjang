37555b489d866e415a63a1c52da76b68d6f74ff1
Davin Byeon <bdv111@snu.ac.kr>
2020-07-13 23:31:48 +0900

    local db & cache setting (#8)

--- nongjang/nongjang/settings.py
+++ nongjang/nongjang/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import json
 
+ENV_MODE = os.getenv('MODE', 'dev')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -80,19 +82,37 @@ secret_file = os.path.join(os.path.dirname(__file__), 'secret_info.json')
 if os.path.exists(secret_file):
     with open(secret_file) as f:
         secret_info = json.loads(f.read())
-        DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.mysql',
-                'NAME': secret_info['DATABASE_NAME'],
-                'USER': secret_info['DATABASE_USER'],
-                'PASSWORD': secret_info['DATABASE_PASSWORD'],
-                'HOST': secret_info['DATABASE_HOST'],
-                'PORT': secret_info['DATABASE_PORT'],
+        if ENV_MODE == 'prod':
+            DATABASES = {
+                'default': {
+                    'ENGINE': 'django.db.backends.mysql',
+                    'NAME': secret_info['DATABASE_NAME'],
+                    'USER': secret_info['DATABASE_USER'],
+                    'PASSWORD': secret_info['DATABASE_PASSWORD'],
+                    'HOST': secret_info['DATABASE_HOST'],
+                    'PORT': secret_info['DATABASE_PORT'],
+                }
             }
-        }
+        else:
+            DATABASES = {
+                'default': {
+                    'ENGINE': 'django.db.backends.mysql',
+                    'NAME': secret_info['DATABASE_NAME'],
+                    'USER': secret_info['DATABASE_USER'],
+                    'PASSWORD': secret_info['DATABASE_PASSWORD'],
+                    'HOST': 'localhost',
+                    'PORT': '3306',
+                }
+            }
 else:
     raise Exception("Check your 'secret_info.json' file!")
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'TIMEOUT': 3600,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
