7f45cc253f1c85111efa1c8ae78b926fa4a83e00
Davin <bdv111@snu.ac.kr>
2020-07-19 16:37:51 +0900

    make first test for App

--- orange/src/App.test.tsx
+++ orange/src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+
 import App from './App';
+import { getMockStore, history } from './tests/mocks';
+
+const mockStore = getMockStore({
+  user: {},
+});
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const app = render(
+    <Provider store={mockStore}>
+      <App history={history} />
+    </Provider>
+  );
+  expect(app.container.firstChild).toHaveClass('App')
 });
---
+++ orange/src/tests/mocks.tsx
@@ -0,0 +1,28 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { connectRouter, routerMiddleware } from "connected-react-router";
+import { createBrowserHistory } from "history";
+
+const getMockReducer = jest.fn(
+    (initialState) => (state: any = initialState, action: any) => {
+        switch (action.type) {
+        default:
+            break;
+        }
+        return state;
+    },
+);
+
+export const history = createBrowserHistory();
+export const middlewares = [thunk, routerMiddleware(history)];
+
+export const getMockStore = (initialState: any) => {
+    const mockUserReducer = getMockReducer(initialState.user);
+    const rootReducer = (history: any) => combineReducers({
+        router: connectRouter(history),
+        user: mockUserReducer,
+    });
+
+    const mockStore = createStore(rootReducer(history), applyMiddleware(...middlewares));
+    return mockStore;
+};
