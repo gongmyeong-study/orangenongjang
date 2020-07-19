100247d70c10724df50c77f24aed9b8b539f77fb
Davin <bdv111@snu.ac.kr>
2020-07-19 16:18:13 +0900

    build

---
+++ orange/build/precache-manifest.fb03089da8e63a1207c3563a90db8880.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322ad33bff09809b89d9eb817921c479",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7eb8e289fada1f9884b9",
+    "url": "/static/css/main.e64da5fc.chunk.css"
+  },
+  {
+    "revision": "5a094da2620890fa9cef",
+    "url": "/static/js/2.46755c67.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.46755c67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eb8e289fada1f9884b9",
+    "url": "/static/js/main.63cd9b40.chunk.js"
+  },
+  {
+    "revision": "d23cc1c25aa0180e5c77",
+    "url": "/static/js/runtime-main.7b9542e5.js"
+  }
+]);
