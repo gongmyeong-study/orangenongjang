83dfcdde4064a604e2ce82ad0155c22f88b406a4
YeonghyeonKo <46114393+YeonghyeonKO@users.noreply.github.com>
2020-08-23 17:07:30 +0900

    Fix remove api (#38)
    * change codes from the latest PR
    
    * show REMOVE_SUCCESS status on console
    
    * delete an unnecessary brace
    
    * build

---
+++ orange/build/precache-manifest.4c7fcebc79221872208374248e29057e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c05412509f773d3ec6de9bd9b58ca01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57b5b0417d7bb7559c97",
+    "url": "/static/css/main.2e708320.chunk.css"
+  },
+  {
+    "revision": "3b698639cf881251f8b3",
+    "url": "/static/js/2.b5367531.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b5367531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57b5b0417d7bb7559c97",
+    "url": "/static/js/main.92b6061e.chunk.js"
+  },
+  {
+    "revision": "1c0ee43c4baba34d129b",
+    "url": "/static/js/runtime-main.fd8f2444.js"
+  }
+]);
