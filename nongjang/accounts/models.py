01a9ec11538191d618c368e3989033643804b8d8
JSKeum <JSKeum2512@gmail.com>
2020-08-30 19:05:18 +0900

    add House & UserHouse model

--- nongjang/accounts/models.py
+++ nongjang/accounts/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class House(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    introduction = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class UserHouse(models.Model):
+    user = models.ForeignKey(User, related_name="houses", on_delete=models.CASCADE)
+    house = models.ForeignKey(House, related_name="users", on_delete=models.CASCADE)
+    leader = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
