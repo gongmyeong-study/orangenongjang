b9c7a5a9259b5b23007a9a2ff91273d3777835dc
JSKeum <JSKeum2512@gmail.com>
2020-09-01 00:36:04 +0900

    add unique_together = ('user', 'house') to UserHouse model & add is_hidden column to House model

--- nongjang/accounts/models.py
+++ nongjang/accounts/models.py
@@ -6,9 +6,13 @@ class House(models.Model):
     name = models.CharField(max_length=50, unique=True)
     introduction = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
+    is_hidden = models.BooleanField(default=False)
 
 class UserHouse(models.Model):
     user = models.ForeignKey(User, related_name="houses", on_delete=models.CASCADE)
     house = models.ForeignKey(House, related_name="users", on_delete=models.CASCADE)
     leader = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'house')
