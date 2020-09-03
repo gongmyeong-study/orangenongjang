cf6c319d279e6dd28c45326c40db71b78817da0a
yeonghyeonKo <dk02315@gmail.com>
2020-09-04 01:00:35 +0900

    build

---
+++ orange/build/precache-manifest.83b075539be387082efc54198726aa68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2150375f76e14e7d2c965fd9da52ad1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27df2bb9885806ee5a9a",
+    "url": "/static/css/main.04f199d6.chunk.css"
+  },
+  {
+    "revision": "5d8c82a2b0a265c710a5",
+    "url": "/static/js/2.73845d60.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.73845d60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27df2bb9885806ee5a9a",
+    "url": "/static/js/main.71596ed5.chunk.js"
+  },
+  {
+    "revision": "1c0ee43c4baba34d129b",
+    "url": "/static/js/runtime-main.fd8f2444.js"
+  }
+]);
