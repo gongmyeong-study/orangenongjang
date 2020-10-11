de2e8a41660c622b69c95135cd1d30765d735b2a
Davin <bdv111@snu.ac.kr>
2020-10-11 22:11:03 +0900

    fix house base logic

--- nongjang/house/models.py
+++ nongjang/house/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class House(models.Model):
     name = models.CharField(max_length=50)
-    introduction = models.CharField(max_length=300)
+    introduction = models.CharField(max_length=300, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     is_hidden = models.BooleanField(default=False)
 
--- nongjang/house/views.py
+++ nongjang/house/views.py
@@ -1,4 +1,4 @@
-from django.db import IntegrityError
+from django.db import IntegrityError, transaction
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
@@ -11,7 +11,7 @@ from necessity.serializers import NecessityLogSerializer
 
 
 class HouseViewSet(viewsets.GenericViewSet):
-    queryset = House.objects.all()
+    queryset = House.objects.filter(is_hidden=False)
     serializer_class = SimpleHouseSerializer
     permission_classes = (IsAuthenticated, )
 
@@ -30,87 +30,79 @@ class HouseViewSet(viewsets.GenericViewSet):
         introduction = request.data.get('introduction')
 
         if not name:
-            return Response({'error': "House 이름을 입력하세요."}, status=status.HTTP_400_BAD_REQUEST)
-        
-        if not House.objects.filter(name=name, is_hidden=False).exists():
+            return Response({'error': "집의 이름을 입력하세요."}, status=status.HTTP_400_BAD_REQUEST)
+
+        with transaction.atomic():
             house = House.objects.create(name=name, introduction=introduction)
-        else:
-            return Response(status=status.HTTP_409_CONFLICT)
-        
-        UserHouse.objects.create(user=user, house=house, leader=True)
+            UserHouse.objects.create(user=user, house=house, leader=True)
 
         return Response(self.get_serializer(house).data, status=status.HTTP_201_CREATED)
 
     # DELETE /api/v1/house/{house_id}/
     def destroy(self, request, pk=None):
         user = request.user
+        house = self.get_object()
 
-        try:
-            house = House.objects.get(pk=pk, is_hidden=False)
-            user_house = UserHouse.objects.get(user=user, house=house)
-        except House.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        except UserHouse.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        
-        if user_house.leader:
-           house.is_hidden = True
-           house.save()
-           return Response(status=status.HTTP_204_NO_CONTENT)
+        user_house = user.user_houses.filter(house=house).last()
+        if not user_house:
+            return Response({'error': "자신의 집만 삭제할 수 있습니다."}, status=status.HTTP_403_FORBIDDEN)
 
+        if not user_house.leader:
+            return Response({'error': "leader만 집을 삭제할 수 있습니다"}, status=status.HTTP_403_FORBIDDEN)
+
+        if house.is_hidden:
+            return Response({'error': "이미 삭제된 집입니다."}, status=status.HTTP_403_FORBIDDEN)
+
+        house.is_hidden = True
+        house.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    # /api/v1/house/{house_id}/user/
+    @action(detail=True, methods=['POST', 'DELETE'])
+    def user(self, request, pk=None):
+        house = self.get_object()
+        if self.request.method == 'POST':
+            return self._join_house(house)
         else:
-          return Response({'error': 'Leader 유저만 House를 삭제할 수 있습니다'}, status=status.HTTP_400_BAD_REQUEST)
-    
-    # POST /api/v1/house/{house_id}/join/
-    @action(detail=True, methods=['post'])
-    def join(self, request, pk=None):
-        user = request.user
+            return self._leave_house(house)
 
-        try:
-            house = House.objects.get(id=pk, is_hidden=False)
-            UserHouse.objects.create(user=user, house=house)
-        except House.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        except IntegrityError:
-            return Response(status=status.HTTP_409_CONFLICT)
-        
+    def _join_house(self, house):
+        user = self.request.user
+        user_house = user.user_houses.filter(house=house).last()
+        if user_house:
+            return Response({'error': "이미 소속되어 있는 집입니다."}, status=status.HTTP_409_CONFLICT)
+
+        UserHouse.objects.create(user=user, house=house)
         return Response(self.get_serializer(house).data)
 
-    # DELETE /api/v1/house/{house_id}/leave/
-    @action(detail=True, methods=['delete'])
-    def leave(self, request, pk=None):
-        user = request.user
+    def _leave_house(self, house):
+        user = self.request.user
 
-        try:
-            user_house = UserHouse.objects.get(user=user, house_id=pk)
-        except UserHouse.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+        user_house = user.user_houses.filter(house=house).last()
+        if not user_house:
+            return Response({'error': "소속되어 있지 않은 집입니다."}, status=status.HTTP_404_NOT_FOUND)
         
-        if not user_house.leader:
-            user_house.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        
-        else:
-            return Response({'error': 'Leader 유저이므로 House를 떠날 수 없습니다. Leader를 다른 User에게 양도한 뒤 다시 시도해 주세요'}, status=status.HTTP_400_BAD_REQUEST)
+        if user_house.leader:
+            return Response({'error': 'leader이므로 집을 떠날 수 없습니다. leader를 다른 유저에게 양도한 뒤 다시 시도해 주세요'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        user_house.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     # GET /api/v1/house/
     def list(self, request):
         user = request.user
-
         houses = House.objects.filter(users__user=user, is_hidden=False)
-
         return Response(self.get_serializer(houses, many=True).data)
 
     # /api/v1/house/{house_id}/necessity/
     @action(detail=True, methods=['POST', 'GET'])
     def necessity(self, request, pk=None):
         house = self.get_object()
-
         if self.request.method == 'POST':
             return self._create_necessity(house)
         return self._get_necessity(house)
 
-    # POST /api/v1/house/{house_id}/necessity/
     def _create_necessity(self, house):
         user = self.request.user
 
@@ -136,7 +128,6 @@ class HouseViewSet(viewsets.GenericViewSet):
 
         return Response(self.get_serializer(house).datta, status=status.HTTP_201_CREATED)
 
-    # GET /api/v1/house/{house_id}/necessity/
     def _get_necessity(self, house):
         return Response(self.get_serializer(house).data)
 
