478d7fee75d51a6e77e249ba262cb8c5a164f968
YeonghyeonKo <46114393+YeonghyeonKO@users.noreply.github.com>
2020-10-21 01:45:16 +0900

    ON-23 [back] modify urls for creating and updating necessity API (#53)
    * modify url for creating and updating necessity API
    
    * add houseId into NecessityOfHouseSerializer
    
    * change NecessityOfHouseSerializer by using source
    
    Co-authored-by: Yeonghyeon <temp@yeonghyeon.local>

--- nongjang/necessity/urls.py
+++ nongjang/necessity/urls.py
@@ -1,12 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
-from necessity.views import NecessityViewSet, NecessityHouseViewSet
+from necessity.views import NecessityViewSet
 
 app_name = 'necessity'
 
 router = SimpleRouter()
 router.register('necessity', NecessityViewSet, basename='necessity')  # /api/v1/necessity/
-router.register('necessity_house', NecessityHouseViewSet, basename='necessity-house')  # /api/v1/necessity_house/
 
 urlpatterns = [
     path('', include((router.urls))),
