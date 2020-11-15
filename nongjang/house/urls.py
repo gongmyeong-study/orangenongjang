482bbbcf862a5ee58921b443ca0f29fb118132e5
Davin Byeon <bdv111@snu.ac.kr>
2020-11-15 16:40:33 +0900

    ON-36 [back] Place 모델 및 관련 API 추가 (#62)
    * necessityplace
    
    * necessitylog handling when necessityplace is deleted
    
    * house 관련 API의 모든 response가 Necessity 정보들을 포함하도록 수정
    
    * list, destroy api는 response 간소화

--- nongjang/house/urls.py
+++ nongjang/house/urls.py
@@ -1,14 +1,16 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
-from house.views import HouseViewSet, HouseNecessityView, HouseNecessityCountView
+from house.views import HouseViewSet, PlaceViewSet, PlaceNecessityView, PlaceNecessityCountView, HousePlaceView
 
 app_name = 'house'
 
 router = SimpleRouter()
 router.register('house', HouseViewSet, basename='house')  # /api/v1/house/
+router.register('place', PlaceViewSet, basename='place')  # /api/v1/place/
 
 urlpatterns = [
     path('', include((router.urls))),
-    path('house/<int:house_id>/necessity/<int:necessity_id>/', HouseNecessityView.as_view()),
-    path('house/<int:house_id>/necessity/<int:necessity_id>/count/', HouseNecessityCountView.as_view()),
+    path('place/<int:place_id>/necessity/<int:necessity_id>/', PlaceNecessityView.as_view()),
+    path('place/<int:place_id>/necessity/<int:necessity_id>/count/', PlaceNecessityCountView.as_view()),
+    path('house/<int:house_id>/place/<int:place_id>/', HousePlaceView.as_view()),
 ]
--- nongjang/necessity/views.py
+++ nongjang/necessity/views.py
@@ -1,4 +1,3 @@
-from django.db import IntegrityError
 from rest_framework import status, viewsets
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
@@ -23,30 +22,10 @@ class NecessityViewSet(viewsets.GenericViewSet):
         일반 유저용의 API가 아님.
         유저가 Necessity를 생성하는 것은 POST /api/v1/house/{house_id}/necessity/ 에서 necessity_id를 포함해 요청하는 경우.
         """
-        data = request.data
-
-        name = data.get('name')
-        if not name:
-            return Response({'error': "name은 필수 항목입니다."}, status=status.HTTP_400_BAD_REQUEST)
-        option = data.get('option', '')
-        description = data.get('description', '')
-        price = data.get('price')
-        if price:
-            if isinstance(price, str):
-                if not price.isnumeric() or int(price) < 0:
-                    return Response({'error': "price는 0 이상의 정수여야 합니다."}, status=status.HTTP_400_BAD_REQUEST)
-                else:
-                    price = int(price)
-            elif not isinstance(price, int) or price < 0:
-                return Response({'error': "price는 0 이상의 정수여야 합니다."}, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            price = None
-
-        try:
-            necessity = Necessity.objects.create(name=name, option=option, description=description, price=price)
-        except IntegrityError:
-            return Response({'error': "이미 존재하는 Necessity 정보입니다."}, status=status.HTTP_409_CONFLICT)
-        return Response(self.get_serializer(necessity).data, status=status.HTTP_201_CREATED)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     # GET /api/v1/necessity/{necessity_id}/
     def retrieve(self, request, pk=None):
