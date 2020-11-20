e37690e3b219ff761eafe754b3d24062b55adc02
JSKeum <JSKeum2512@gmail.com>
2020-11-20 22:28:13 +0900

    build

--- orange/build/service-worker.js
+++ orange/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a559f22237cc702cc451b9872c2018a2.js"
+  "/precache-manifest.077320b13a66b87a1e6c2ab4640af133.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
