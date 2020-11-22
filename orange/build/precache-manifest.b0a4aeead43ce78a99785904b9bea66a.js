30486ba1b515663ac9dcdae04bb799d3741686c2
Yeonghyeon <temp@yeonghyeon.local>
2020-11-22 22:25:05 +0900

    build

---
+++ orange/build/precache-manifest.b0a4aeead43ce78a99785904b9bea66a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93c1ed3d0853ece7821528a2a58389f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd4070d7f09bb599fcaf",
+    "url": "/static/css/main.fd390d4d.chunk.css"
+  },
+  {
+    "revision": "ac2d29d70115dc6b9b24",
+    "url": "/static/js/2.327811cc.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.327811cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd4070d7f09bb599fcaf",
+    "url": "/static/js/main.50cef626.chunk.js"
+  },
+  {
+    "revision": "1c0ee43c4baba34d129b",
+    "url": "/static/js/runtime-main.fd8f2444.js"
+  }
+]);
--- orange/build/service-worker.js
+++ orange/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.077320b13a66b87a1e6c2ab4640af133.js"
+  "/precache-manifest.b0a4aeead43ce78a99785904b9bea66a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
