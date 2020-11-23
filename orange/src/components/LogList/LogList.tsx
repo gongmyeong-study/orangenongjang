90a751d2437a191511c314778b11d59903209f42
YeonghyeonKo <46114393+YeonghyeonKO@users.noreply.github.com>
2020-11-24 00:38:12 +0900

    ON-38 [front] Timeline 예쁘게 꾸미기 (#67)
    * rendering timeline with material-ui
    
    * decorate with icons for each activity
    
    * add new line in TimelinePage.css
    
    * reflect reviews
    
    Co-authored-by: Yeonghyeon <temp@yeonghyeon.local>

--- orange/src/components/LogList/LogList.tsx
+++ orange/src/components/LogList/LogList.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+
+import AutorenewIcon from '@material-ui/icons/Autorenew';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import ExposureRoundedIcon from '@material-ui/icons/ExposureRounded';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import Paper from '@material-ui/core/Paper';
+import Timeline from '@material-ui/lab/Timeline';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
+import Typography from '@material-ui/core/Typography';
+
 import { NecessityLog } from '../../api';
+import './LogList.css';
 
 interface Props {
   logs: NecessityLog;
@@ -13,34 +28,62 @@ function LogList(props: Props) {
   const activityCategory = () => {
     switch (props.logs.action) {
       case 'CREATE':
-        return '을(를) 생필품 목록에 추가했습니다.';
+        return '이/가 생필품 목록에 추가되었습니다.';
       case 'UPDATE':
-        return '을(를) 수정했습니다.';
+        return '이/가 수정되었습니다.';
       case 'DELETE':
-        return '을(를) 생필품 목록에서 삭제했습니다.';
+        return '이/가 생필품 목록에서 삭제되었습니다.';
       case 'COUNT':
-        return '의 수량을 변경했습니다.';
+        return '의 수량이 변경되었습니다.';
       default:
-        return '을(를) 수정했습니다.';
+        return '이/가 수정되었습니다.';
+    }
+  };
+
+  const activityIcon = () => {
+    // eslint-disable-next-line react/prop-types
+    switch (props.logs.action) {
+      case 'CREATE':
+        return <ShoppingCartIcon />;
+      case 'UPDATE':
+        return <AutorenewIcon />;
+      case 'DELETE':
+        return <DeleteForeverIcon />;
+      case 'COUNT':
+        return <ExposureRoundedIcon />;
+      default:
+        return <AutorenewIcon />;
     }
   };
 
   return (
     <div className="logList">
-      <p>
-        {username}
-        {' '}
-        이(가)
-        {' '}
-        &apos;
-        {necessityname}
-        &apos;
-        {activityCategory()}
-        {' '}
-        (
-        {createdAt}
-        )
-      </p>
+      <Timeline>
+        <TimelineItem>
+          <TimelineOppositeContent>
+            <Typography color="textSecondary">{createdAt}</Typography>
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            {activityIcon()}
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent>
+            <Paper elevation={3} className="log-message">
+              <Typography variant="h6" component="h1">
+                <b>{necessityname}</b>
+                {activityCategory()}
+                {' '}
+              </Typography>
+              <Typography>
+                (
+                {username}
+                )
+              </Typography>
+            </Paper>
+            <Typography />
+          </TimelineContent>
+        </TimelineItem>
+      </Timeline>
     </div>
   );
 }
