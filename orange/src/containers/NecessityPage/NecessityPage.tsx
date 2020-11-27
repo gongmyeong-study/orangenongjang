82a9ad3e95d5fa9bdc1c4c914b2af57a43828e0a
JSKeum <JSKeum2512@gmail.com>
2020-11-28 01:43:10 +0900

    replace connect with hook

--- orange/src/containers/NecessityPage/NecessityPage.tsx
+++ orange/src/containers/NecessityPage/NecessityPage.tsx
@@ -1,47 +1,40 @@
-import React, { Dispatch, useEffect, useState } from 'react';
-import { connect, useDispatch, useSelector } from 'react-redux';
-import { History } from 'history';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Slider from 'react-slick';
 import Modal from 'react-modal';
-import { TextField, InputAdornment, Button } from '@material-ui/core';
-import { useForm } from 'react-hook-form';
+import { History } from 'history';
+import { Button } from '@material-ui/core';
 import { PlaceBox, PlaceCreateForm } from '../../components';
-import { necessityActions } from '../../store/actions';
-import { Place } from '../../api';
 import { OrangeGlobalState } from '../../store/state';
 import './NecessityPage.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { createPlace } from '../../store/actions/necessity/necessity';
+import { getHouse } from '../../store/actions/necessity/necessity';
 import { necessityStatus } from '../../constants/constants';
 
 interface Props {
-  history: History;
-  onGetHouse(houseId: number): void;
-  places: Place[];
   houseId: number;
-}
-
-interface PlaceCreateFormData {
-  name: string;
+  history: History;
 }
 
 function NecessityPage(props: Props) {
   const [isModalOpen, setModalOpen] = useState(false);
 
-  const { createStatus } = useSelector((state: OrangeGlobalState) => state.necessity);
+  const { createStatus, places } = useSelector((state: OrangeGlobalState) => state.necessity);
   const dispatch = useDispatch();
 
-  const fetchHouse = () => {
-    props.onGetHouse(props.houseId);
+  const onGetHouse = (
+    houseId: number,
+  ) => {
+    dispatch(getHouse(houseId));
   };
 
   useEffect(() => {
-    fetchHouse();
+    onGetHouse(props.houseId);
     if (createStatus === necessityStatus.SUCCESS) {
       setModalOpen(false);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [createStatus]);
 
   return (
@@ -57,7 +50,7 @@ function NecessityPage(props: Props) {
       <div className="placebox-wrapper">
         <h1 className="NecessityPageBlock">집집</h1>
         <Slider className="slider" dots slidesToShow={1.6} slidesToScroll={1} infinite={false}>
-          {Boolean(props.places) && props.places.map((place) => <PlaceBox place={place} />)}
+          {Boolean(places) && places.map((place) => <PlaceBox place={place} />)}
           <div
             className="create-place-box"
             style={{
@@ -75,14 +68,4 @@ function NecessityPage(props: Props) {
   );
 }
 
-const mapStateToProps = (state: OrangeGlobalState) => ({
-  places: state.necessity.places,
-});
-
-const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
-  onGetHouse: (houseId: number) => dispatch(
-    necessityActions.getHouse(houseId),
-  ),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(NecessityPage);
+export default NecessityPage;
