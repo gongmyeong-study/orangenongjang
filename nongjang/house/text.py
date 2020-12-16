0b56ccede3ae3ca53f0baedea57bc1f60c653995
YeonghyeonKO <dk02315@gmail.com>
2020-12-17 02:48:44 +0900

    House invitation Authentication by email

---
+++ nongjang/house/text.py
@@ -0,0 +1,14 @@
+import os
+
+
+ENV_MODE = os.getenv('MODE', 'dev')
+
+
+def house_invite_message(domain, house_uidb64, user_uidb64, token, user, house, invited_user):
+    link = f"http://{domain}/api/v1/house/{house_uidb64}/user/{user_uidb64}/activate/{token}/"
+
+    return f"To. {invited_user}\n\n" \
+           f"{user.username}님이 {house.name} 초대장을 전송했습니다!\n" \
+           f"아래 링크를 클릭하면 {house.name}에 초대됩니다.\n\n" \
+           f"링크 : {link}\n\n" \
+           f"From. 오렌지농장(orangenongjang)"
