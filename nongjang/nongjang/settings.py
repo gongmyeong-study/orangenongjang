b00ccea4067cf3e2def68814fcbb1dd53c448246
Davin Byeon <bdv111@snu.ac.kr>
2021-01-17 01:08:19 +0900

    deploying django static files (#88)

--- nongjang/nongjang/settings.py
+++ nongjang/nongjang/settings.py
@@ -176,6 +176,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 if USE_DEBUG_TOOLBAR:
     INSTALLED_APPS += (
