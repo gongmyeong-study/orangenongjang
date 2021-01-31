1ca4c12b92453e36777a8c26ab1b2074d0e99cee
yeonghyeonKO <dk02315@gmail.com>
2021-01-31 15:52:01 +0900

    decorate signup/invitation text when receiving mails

--- nongjang/house/text.py
+++ nongjang/house/text.py
@@ -1,8 +1,8 @@
 def house_invite_message(domain, house_uidb64, user_uidb64, token, user, house, invited_user):
     link = f"{domain}/{house_uidb64}/user/{user_uidb64}/activate/{token}/"
 
-    return f"To. [ {invited_user} ]\n\n" \
-           f" [ {user.username} ]님이 [ {house.name} ] 초대장을 전송했습니다!\n" \
+    return f"To. [ {invited_user} ]\n\n\n\n" \
+           f" [ {user.username} ]님이 [ {house.name} ] 초대장을 전송했습니다!\n\n" \
            f"아래 링크를 클릭하면 [ {house.name} ]에 초대됩니다.\n\n" \
-           f"링크 : {link}\n\n" \
+           f"링크 : {link}\n\n\n\n" \
            f"From. 오렌지농장(orangenongjang)"
--- nongjang/user/text.py
+++ nongjang/user/text.py
@@ -4,6 +4,5 @@ def user_invite_message(domain, uidb64, token, user):
     return f"To. [ {user} ]\n\n\n\n" \
            f"아래 링크를 클릭하면 회원 인증이 완료됩니다.\n\n" \
            f"링크 : {link}\n\n" \
-           f"오렌지농장에 오신 것을 환영합니다 :)\n\n" \
+           f"오렌지농장에 오신 것을 환영합니다 :)\n\n\n\n" \
            f"From. 오렌지농장(orangenongjang)"
-
