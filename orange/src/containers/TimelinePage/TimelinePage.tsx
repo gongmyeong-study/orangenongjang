79996e0b4a7fe75a9da6609c803967c156c47905
Davin <bdv111@snu.ac.kr>
2021-02-21 12:20:42 +0900

    check logList length

--- orange/src/containers/TimelinePage/TimelinePage.tsx
+++ orange/src/containers/TimelinePage/TimelinePage.tsx
@@ -93,12 +93,13 @@ class TimelinePage extends Component<Props, State> {
         <LogList key={log.id} logs={log} />
       </div>
     ));
-
     const logMessage = <h2>여기에 생필품 관련 기록이 쌓여요.</h2>;
 
     return (
       <div className="timeline-page">
-        {getLogStatus === necessityUserLogStatus.SUCCESS ? logList : logMessage}
+        {getLogStatus === necessityUserLogStatus.SUCCESS && logList.length
+          ? logList
+          : logMessage}
       </div>
     );
   }
